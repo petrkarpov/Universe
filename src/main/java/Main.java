f928fdd955b0df48faae7f750d94180c97bdbc29
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-01 18:11:12 +0300

    Universe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        final long AGE_UNIVERSE = 13000000000L;
-        final int AGE_OF_HUMANITY = 100000;
+        final long ageUniverse= 13000000000L;
+        final int ageOfHumanity = 100000;
 
-        final long AGE_DIFFERENCE = AGE_UNIVERSE / AGE_OF_HUMANITY;
+        final long ageDifference = ageUniverse / ageOfHumanity;
 
-        System.out.println("Вселенная старше людей в: " + AGE_DIFFERENCE + " раз");
+        System.out.println("Вселенная старше людей в: " + ageDifference + " раз");
     }
 }
