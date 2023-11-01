19f1ac0a0377cef6d5e016fbce2d210d52ec1c62
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-01 18:54:20 +0300

    Universe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        final long ageUniverse= 13000000000L;
+        final long ageUniverse = 13000000000L;
         final int ageOfHumanity = 100000;
         final long ageDifference = ageUniverse / ageOfHumanity;
 
